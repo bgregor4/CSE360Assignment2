914034bf05e1876a2f253ed030df308075bfd323
bgregor4 <bgregor4@gmail.com>
2020-10-05 22:12:59 -0700

    Second Version : includes main file that has test cases

--- assignment2/src/cse360assignment02/AddingMachine.java
+++ assignment2/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,35 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  
+  private String memory;
+  //total keeps track of the transaction amount
+  //memory keeps track of the history of the transactions
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    memory = "0";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total = total + value;
+	  memory = memory + " + " + value;
+	  
   }
 
   public void subtract (int value) {
+	  total = total - value;
+	  memory = memory + " - " + value;
+	  
   }
 
   public String toString () {
-    return "";
+    return memory;
   }
 
   public void clear() {
+	  memory = "";
   }
 }
