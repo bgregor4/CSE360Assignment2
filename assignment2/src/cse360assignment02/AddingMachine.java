a5c30cc363ddd9e262d5daefbff51b2d2e3611d0
bgregor4 <bgregor4@asu.edu>
2020-10-05 22:17:50 -0700

    Adding links

--- assignment2/src/cse360assignment02/AddingMachine.java
+++ assignment2/src/cse360assignment02/AddingMachine.java
@@ -3,7 +3,13 @@ package cse360assignment02;
 public class AddingMachine {
   private int total;
   private String memory;
-  //total keeps track of the transaction amount
+  
+  //Link to github repo
+  //https://github.com/bgregor4/CSE360Assignment2
+  //username: bgregor4
+  //email: bgregor4@asu.edu
+  //Bryson Gregory
+  //total keeps track of the transaction  amount
   //memory keeps track of the history of the transactions
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
